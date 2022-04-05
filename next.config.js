c726dc780268ce5176a58f145a794137cd7170c3
sagarjethideveloper <you@example.com>
2022-04-05 13:32:25 +0530

    update read me

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     reactStrictMode: true,
+    eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: false,
+   },
+   swcMinify: false ,// ,
     async redirects() {
         return [
             {
